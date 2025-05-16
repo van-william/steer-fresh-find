234482c946311c8f3200fc0fde8a0453d4105162
Matt LeMay <mplemay@users.noreply.github.com>
2025-05-16 16:20:31 -0500

    refactor: migrate to vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   appDirectory: "src",
   ssr: true,
   future: { unstable_viteEnvironmentApi: true },
+  presets: [vercelPreset()],
 } satisfies Config;
