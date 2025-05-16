7d128e48d3c28bca6a00ed0524f97b1958802a29
Matt LeMay <mplemay@users.noreply.github.com>
2025-05-16 12:07:36 -0500

    feat: added a basic login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { supabase } from "@/lib/supabase";
+
+export default function App() {
+  const [session, setSession] = useState(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+  if (!session) {
+    return (
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google", "facebook"]}
+      />
+    );
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
