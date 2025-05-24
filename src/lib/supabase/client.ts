133fee2f31757260578ead0ada3901e058188130
Matt LeMay <mplemay@users.noreply.github.com>
2025-05-24 17:38:37 -0500

    feat: added database types

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
+import type { Database } from "@/lib/supabase/types";
 
 export function createClient() {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     import.meta.env.VITE_SUPABASE_URL!,
     import.meta.env.VITE_SUPABASE_ANON_KEY!
-  )
+  );
 }
